af71fbd64941b25362b5fc21c7bb178e99b6e21c
ZTianWang <wnwn1994@163.com>
2022-04-25 18:29:37 +0800

    complete

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Axios-js-demo
 axios的纯JS版
+
+## 安装axios:
+`yarn add axios`
+
+## 安装json-server
+`yarn add json-server`
+
+## 运行json-json
+`yarn json-server --watch db.json`
---
+++ app.js
@@ -0,0 +1,7 @@
+// const axios = require('axios')
+
+let btn = document.getElementById('btn')
+let box = document.getElementById('resBox')
+btn.addEventListener('click', () => {
+    axios.get("")
+})
